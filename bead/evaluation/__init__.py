000c235711b43aa4821391124e2ea69c6292447f
Aaron Steven White <aaronstevenwhite@gmail.com>
2026-01-23 15:42:38 -0500

    Removes obsolete cross_validation and model_metrics evaluation modules

--- bead/evaluation/__init__.py
+++ bead/evaluation/__init__.py
@@ -5,13 +5,9 @@ performance metrics, and convergence detection for active learning.
 """
 
 from bead.evaluation.convergence import ConvergenceDetector
-from bead.evaluation.cross_validation import CrossValidator
 from bead.evaluation.interannotator import InterAnnotatorMetrics
-from bead.evaluation.model_metrics import ModelMetrics
 
 __all__ = [
-    "CrossValidator",
     "InterAnnotatorMetrics",
-    "ModelMetrics",
     "ConvergenceDetector",
 ]
